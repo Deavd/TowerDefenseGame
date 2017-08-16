d1b9f4c131671aaf6fd4160f496b2e9844567acd
David <david.boehm@mng.ch>
2017-08-17 01:53:22 +0200

    +can now die (receive dmg and heal)

--- Assets/TDF/Scripts/Enemies/Enemy.cs
+++ Assets/TDF/Scripts/Enemies/Enemy.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		this.GetComponent<NavMeshAgent>().speed = speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	float health;
-	float speed;
+	public float health = 100;
+	public float speed = 10;
+	public float reward = 100;
 
+	public int damage = 1;
+	public void ReceiveDamage(float dmg){
+		health -= dmg;
+		if(health <= 0){
+			Die();
+		}
+		//damage animation
+	}
+	public void Heal(float amount){
+		ReceiveDamage(-amount);
+	}
+	public void Die(){
+		Destroy(this.gameObject);
+		LevelHandler.Instance.Money += reward;
+		//die animation
+	}
+	public void Destinated(){
+		Destroy(this.gameObject);
+		LevelHandler.Instance.Lifes -= damage;
+	}
 }
