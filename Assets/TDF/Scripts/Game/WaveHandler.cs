3e9f40fe20c6c6bf7341df10f551b9f6c8bf8b3e
David <david.boehm@mng.ch>
2017-08-17 17:54:19 +0200

    + added spawning waves (only one wave atm)

--- Assets/TDF/Scripts/Game/WaveHandler.cs
+++ Assets/TDF/Scripts/Game/WaveHandler.cs
@@ -21,32 +21,27 @@ public class WaveHandler : MonoBehaviour
     public GameObject[] spawnpoints;
     int difficulty = 1;
     int wave = 0;
-    public void spawnWave()
+
+      bool canSpawn = true;
+     private float nextSpawn = 0.0f;
+    public float period = 1.1f;
+    void Update(){
+        if (Time.time > nextSpawn ) {
+            nextSpawn = Time.time + period;
+        }
+
+    }
+    public void startSpawningWaves()
     {
         wave++;
-        calculateRandomWave(wave, difficulty);
-        /*for(int x = 0; x < 100; x++){
-            calculateRandomWave(x+1,1);
-            calculateRandomWave(x+1,2);
-            calculateRandomWave(x+1,3);
-            Debug.Log("");
-        }*/
-    }
-    private void calculateRandomWave(int wave, int difficulty)
-    {
-        int amount = calculateAmount(wave, difficulty);
-        for(int i = 0; i < amount; i++){
-            GameObject enemy = Instantiate(enemies[0], spawnpoints[0].transform.position, Quaternion.identity);
-            enemy.GetComponent<NavMeshAgent>().SetDestination(homeBase.transform.position);
-        }
-        /*difficulty 
-        1
-        Wave 1
-        difficulty 1    -> more loot
-                        -> enemies have less health
-                        -> enemies are dumb?*/
-        Debug.Log(calculateAmount(wave, difficulty) + " [" + difficulty + ", " + wave + "]");
+        spawnRandomWave(wave, difficulty);
 
+    }
+    private void spawnRandomWave(int wave, int difficulty)
+    {
+        String s = "0:10:1 0:10:2 0:100:0.2";
+
+        StartCoroutine("SpawnWave", s);
     }
     private int calculateAmount(double wave, double difficulty)
     {
@@ -56,5 +51,41 @@ public class WaveHandler : MonoBehaviour
         int z = Convert.ToInt32((x / y + 1) * wave * 1.8);
         return z;
     }
+        bool isRunning = false;    
+    void Upadte(){
+
+    }
+    float timeToStart = 2;
+    float delayToNextPartWave = 0.5f;
+    float timeToNextPartWave = 0;
+    IEnumerator SpawnWave(String s){
+        yield return new WaitForSeconds(timeToStart);
+        Debug.Log("Spawning Wave");
+        isRunning = true;
+        //String format: id:amount:period id:amount:period
+        String[] partWaves = s.Split();
+        foreach(String partWave in partWaves){
+            Debug.Log("Spawning PartWave");
+            String[] parts = partWave.Split(":".ToCharArray()[0]);
+            int id;
+            int.TryParse(parts[0], out id);
+            int amount;
+            int.TryParse(parts[1], out amount);
+            float period;
+            float.TryParse(parts[2], out period);
+            for(int i = 0; i < amount; i++){
+                GameObject spawnedEnemy = Instantiate(enemies[id], spawnpoints[0].transform.position, Quaternion.identity);
+                spawnedEnemy.GetComponent<NavMeshAgent>().SetDestination(homeBase.transform.position);
+                Debug.Log("Spawning Enemy with "+period + "delay");
+                yield return new WaitForSeconds(period);
+            }
+            yield return new WaitForSeconds(delayToNextPartWave);
+        }
+        isRunning = false;
+    }
+
+}
+public class Wave : MonoBehaviour
+{
 
 }
