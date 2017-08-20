4123534e33ca770cc0fdbed899165461fafa0fe5
David <david.boehm@mng.ch>
2017-08-20 15:08:01 +0200

    added rocket tower

---
+++ Assets/TDF/Scripts/Towers/RocketlauncherTower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketlauncherTower : Tower {
+    public override void AttackEnemy(){
+        if(target == null){return;}
+        Transform spawnPoints = this.transform.GetChild(0).GetChild(0);
+        GameObject missile = (GameObject) Instantiate(Missile, spawnPoints.GetChild(0).position, this.transform.GetChild(0).rotation);
+		GameObject missile2 = (GameObject) Instantiate(Missile, spawnPoints.GetChild(1).position, this.transform.GetChild(0).rotation);
+        missile.GetComponent<Missiles>().Shoot(target, Damage/2);    
+		missile2.GetComponent<Missiles>().Shoot(target, Damage/2);   
+
+    }
+}
