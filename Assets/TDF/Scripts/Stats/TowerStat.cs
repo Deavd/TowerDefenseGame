eb73a08a138171703917e01383cfbc6298e4b209
David <david.boehm@mng.ch>
2017-08-21 23:50:10 +0200

    +all project data

---
+++ Assets/TDF/Scripts/Stats/TowerStat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerStats : MonoBehaviour {
+	List<Stat> towerStats = new List<Stat>();
+	// Use this for initialization
+	void Start () {
+		towerStats.Add(new Stat("Health", 100, "", Color.green));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
