1ab44b9eda00e66519f209e4e78fe16a7295b5f6
David <david.boehm@mng.ch>
2017-10-22 01:47:34 +0200

    adjusted move range and smooth movement

--- Assets/TDF/Scripts/Camera/CameraMovement.cs
+++ Assets/TDF/Scripts/Camera/CameraMovement.cs
@@ -3,53 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		dest = Camera.main.transform.position;
-	}
-	
-	// Update is called once per frame
 	float zoom = 9f;
-	float maxZoom = 12f;
-	float minZoom = 3f;
+	public float maxZoom = 12f;
+	public float minZoom = 3f;
 	float zoomSpeed = 10f;
 	float speed = 0.2f;
-	float moveOffset = 0f;
+	float moveOffset = 1f;
 
 	float minX = 0;
 	float minZ = -1;
-	float maxX = 12;
-	float maxZ =  12;
+	float maxX = 19;
+	float maxZ =  5;
 
 	float distance;
 	Vector3 dest;
-	void Update () {
+	void Start () 
+	{
+		dest = Camera.main.transform.position;
+	}
+	void Update () 
+	{
 		Vector2 mousePos = Input.mousePosition;
 		int x = (int) mousePos.x;
 		int y = (int) mousePos.y;
-		if(mousePos.x < moveOffset){
-			//mousepos is at the botton
-			//move down
+
+		if(mousePos.x < moveOffset)
+		{
 			dest.x = Mathf.Clamp(dest.x-speed, minX, maxX);
-		}else if(x > Screen.width - moveOffset) {
+		}
+		else if(x > Screen.width - moveOffset) 
+		{
 			dest.x = Mathf.Clamp(dest.x+speed, minX, maxX);
 		}
-		if(mousePos.y < moveOffset){
-			//mousepos is at the botton
+		if(mousePos.y < moveOffset)
+		{
+			//mousepos is at the bottom
 			//move down
 			dest.z = Mathf.Clamp(dest.z-speed, minZ, maxZ);
-		}else if(y > Screen.height - moveOffset) {
+		}
+		else if(y > Screen.height - moveOffset) 
+		{
 			dest.z = Mathf.Clamp(dest.z+speed, minZ, maxZ);
 		}
-		if(Input.GetAxis("Mouse ScrollWheel") != 0f){
+		if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+		{
 			zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
      		zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
 			dest.y = zoom;		
 		}
 		Vector3 s = transform.position-dest;
-		if(s.magnitude >= 0.02){
-        	transform.position = Vector3.Lerp(transform.position, dest, 0.1f);
+		if(s.magnitude >= 0.02)
+		{
+        	transform.position = Vector3.Lerp(transform.position, dest, 4f * Time.deltaTime);
 		}
 	}
 
