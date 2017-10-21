3a14d8415ac3d952c87ba2156f99c175a18c6e60
David <david.boehm@mng.ch>
2017-10-22 01:58:25 +0200

    a modifier for stats, which is able to expire

---
+++ Assets/TDF/Scripts/Stats/StatModifier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatModifierAddType{
+	PERCENTAGE_BASE,
+	VALUE_BASE_PERMANENT,
+	PERCENTAGE_MODIFIED,
+	VALUE_MODIFIED
+}
+public class StatModifier{
+	public float Time{set;get;}
+	public bool hasTime{
+		get{
+			return Time != 0;
+		}
+	}
+	public float Value{set;get;}
+	public StatModifierAddType modifierAddType;
+	public StatModifier(StatModifierAddType addType, float value)
+	{
+		this.modifierAddType = addType;
+		this.Value = value;
+	}
+	public StatModifier withTime(float time)
+	{
+		this.Time = time;
+		return this;
+	}
+}
