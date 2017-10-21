e3d7f671344aede4a8bc132fd4a961baa1c02be0
David <david.boehm@mng.ch>
2017-10-22 01:56:41 +0200

    added stats for enemy

---
+++ Assets/TDF/Scripts/Stats/Managers/EnemyStat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : StatManager {
+
+	public Stat Speed;
+	public Stat Damage;
+	public Stat Health;
+	public Stat MaxHealth;
+	public Stat Reward;
+	protected override void Awake () {
+		addStat(StatType.MaxHealth, MaxHealth);
+		addStat(StatType.Health, Health);
+		addStat(StatType.Speed, Speed);
+		addStat(StatType.Damage, Damage);
+		addStat(StatType.Reward, Reward);
+		base.Awake();
+	}
+}
