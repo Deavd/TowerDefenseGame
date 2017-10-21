481e4425b2ac49fe20fca3a89380ccbaf569d545
David <david.boehm@mng.ch>
2017-10-22 01:51:39 +0200

    OnPointerClick instead of OnMouseDown event (for better control)

--- Assets/TDF/Scripts/Towers/MapObject.cs
+++ Assets/TDF/Scripts/Towers/MapObject.cs
@@ -1,46 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public enum MapObjectType
 {
     GROUND, SPAWNPOINT, BASE, TOWER
 }
-public class MapObject : MonoBehaviour
+public class MapObject : MonoBehaviour, IPointerClickHandler
 {
     public int posX, posZ;
     public bool isBuildable = true;
     public bool isWalkable = true;
     MapObjectType type = MapObjectType.GROUND;
-    GameObject tower;
-    void OnMouseDown()
-    {
-        if (LevelHandler.Instance.selectedTower != -1)
-        {
-            if (isBuildable)
-            {
-                GameObject TowerObj = LevelHandler.Instance.PlaceTower(posX, posZ);
-                if (TowerObj != null)
-                {
-                    this.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
-					isBuildable = false;
-                    this.tower = TowerObj;
-                }
-            }
-        }else{
-            if(tower != null){
-                Tower t = tower.GetComponent<Tower>();
-                UIHandler.Instance.LoadTowerGui(t);
-            }
-        }
+    public Tower Tower;
+    private Color color;
+    void Start(){
+        color = this.GetComponent<Renderer>().material.color;
     }
+
     void OnMouseExit()
     {
-		this.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
+		this.GetComponent<Renderer>().material.color = color;
     }
     void OnMouseEnter()
     {
-        if (LevelHandler.Instance.selectedTower != -1)
+        if (BuildManager.selectedTower != -1)
         {
 			if(isBuildable){
             	this.GetComponent<Renderer>().material.color = new Color(0, 1, 0, 1);
@@ -51,6 +37,31 @@ public class MapObject : MonoBehaviour
         else
         {
             this.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f, 1);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (BuildManager.selectedTower != -1)
+        {
+            TowerMenuUI.Instance.UnloadTowerGui();
+            if (isBuildable)
+            {
+                Tower towerObj = BuildManager.Instance.PlaceTower(posX, posZ);
+                if (towerObj != null)
+                {
+                    this.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
+					isBuildable = false;
+                    this.Tower = towerObj;
+                    TowerMenuUI.Instance.LoadTowerGui(this);
+                }
+            }
+        }else{
+            if(Tower != null){
+                TowerMenuUI.Instance.LoadTowerGui(this);
+            }else{
+                TowerMenuUI.Instance.UnloadTowerGui();
+            }
         }
     }
 }
