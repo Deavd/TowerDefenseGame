9759ddbfb6a1b170a37701cadf987463ce7b2d47
David <david.boehm@mng.ch>
2017-10-22 01:49:14 +0200

    implemented stat system

--- Assets/TDF/Scripts/Enemies/Enemy.cs
+++ Assets/TDF/Scripts/Enemies/Enemy.cs
@@ -4,40 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour {
+[RequireComponent(typeof(EnemyStat))]
+public class Enemy : MonoBehaviour{
 
 	// Use this for initialization
-	void Start () {
+	public EnemyStat Stats;
+	protected virtual void Awake () {
+		Stats = GetComponent<EnemyStat>();
+	}
+	protected virtual void Start () {
 		InitStart();
 	}
 	public GameObject healthBar;
-	public Image healthBarImage;
+	private Image healthBarImage;
 	public void InitStart () {
-		this.GetComponent<NavMeshAgent>().speed = speed;
-		healthBar = Instantiate(LevelHandler.healthBar);
+		this.GetComponent<NavMeshAgent>().speed = Stats.Speed.Value;
+		healthBar = Instantiate(LevelManager.Instance.healthBar);
 		healthBarImage = healthBar.transform.GetChild(0).GetComponent<Image>();
 
-		healthBar.transform.SetParent(LevelHandler.GameUI.transform, false);
+		healthBar.transform.SetParent(LevelManager.Instance.GameUI.transform, false);
 		healthBar.transform.position = Camera.main.WorldToScreenPoint(this.transform.position);
-		health = maxHealth;
+		Stats.Health.BaseValue = Stats.MaxHealth.Value;
 	}
 	
 	// Update is called once per frame
 	public void OnUpdate () {
-		healthBar.transform.position = LevelHandler.mainCamera.WorldToScreenPoint(this.transform.position+Vector3.up);
+		healthBar.transform.position = LevelManager.mainCamera.WorldToScreenPoint(this.transform.position+Vector3.up);
 	}
-	public float maxHealth = 100;
-	public float health;
-	public float speed = 10;
-	public float reward = 100;
-
-	public int damage = 1;
 	public void ReceiveDamage(float dmg){
-		health -= dmg;
-		if(health <= 0){
+		Stats.Health.AddBase(-dmg);
+		if(Stats.Health.Value <= 0){
 			Die();
 		}
-		healthBarImage.fillAmount = health/maxHealth;
+		healthBarImage.fillAmount = Stats.Health.Value/Stats.MaxHealth.Value;
 		//damage animation
 	}
 	public void Heal(float amount){
@@ -46,12 +45,12 @@ public class Enemy : MonoBehaviour {
 	public void Die(){
 		Destroy(this.gameObject);
 		Destroy(healthBar);
-		LevelHandler.Instance.Money += reward;
+		LevelManager.Instance.Money += Stats.Reward.Value;
 		//die animation
 	}
 	public void Destinated(){
 		Destroy(this.gameObject);
 		Destroy(healthBar);
-		LevelHandler.Instance.Lifes -= damage;
+		LevelManager.Instance.Lifes -= (int)Stats.Damage.Value;
 	}
 }
