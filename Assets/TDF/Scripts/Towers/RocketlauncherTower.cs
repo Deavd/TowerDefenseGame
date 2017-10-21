708c1f4f79ee2d0192f13cfb710e1b71eeb7f7ce
David <david.boehm@mng.ch>
2017-10-22 01:51:59 +0200

    no message

--- Assets/TDF/Scripts/Towers/RocketlauncherTower.cs
+++ Assets/TDF/Scripts/Towers/RocketlauncherTower.cs
@@ -1,15 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RocketlauncherTower : Tower {
+
     public override void AttackEnemy(){
-        if(target == null){return;}
+        if(Target == null){return;}
         Transform spawnPoints = this.transform.GetChild(0).GetChild(0);
         GameObject missile = (GameObject) Instantiate(Missile, spawnPoints.GetChild(0).position, this.transform.GetChild(0).rotation);
 		GameObject missile2 = (GameObject) Instantiate(Missile, spawnPoints.GetChild(1).position, this.transform.GetChild(0).rotation);
-        missile.GetComponent<Missiles>().Shoot(target, Damage/2);    
-		missile2.GetComponent<Missiles>().Shoot(target, Damage/2);   
-
+        //missile.GetComponent<Missiles>().Shoot(Target, Damage/2);    
+        //missile.GetComponent<Missiles>().Shoot(Target, Damage/2);    
+		missile2.GetComponent<Missiles>().Shoot(Target, Stats.Damage.Value/2);   
+		missile2.GetComponent<Missiles>().Shoot(Target, Stats.Damage.Value/2);       
     }
 }
