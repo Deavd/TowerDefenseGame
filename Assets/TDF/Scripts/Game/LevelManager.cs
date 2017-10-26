288568fbe087bf5900af58329c68fba35944cf50
David <david.boehm@mng.ch>
2017-10-26 23:35:06 +0200

    added defeat effect (with post processing asset) and the possibility to pause the game and switch screens (GameOver, Pause, Game)

--- Assets/TDF/Scripts/Game/LevelManager.cs
+++ Assets/TDF/Scripts/Game/LevelManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.PostProcessing;
 public class LevelManager : MonoBehaviour
 {
     private static LevelManager _instance;
@@ -26,6 +27,7 @@ public class LevelManager : MonoBehaviour
     private float startTime;                //timeTheLevelStarted
     private float disTime;
 
+    public static bool IsGameOver { get; set; }
     public static bool IsPaused { get; set; }
 
     private double _money = 600;
@@ -52,44 +54,117 @@ public class LevelManager : MonoBehaviour
         {
             _lifes = value;
             if(_lifes <= 0){
-                Defeat();
                 value = 0;
+            }
+            if(value == 0 && !IsGameOver){
+                Defeat();
             }
             gameLifesText.text = value.ToString();
         }
     }
     void Awake()
     {
+        Lifes = _lifes;
+
         mainCamera =  Camera.main;
         healthBar =  GameObject.Find("Healthbar");
         GameUI = GameObject.Find("GameUI").GetComponent<Canvas>();
-        Debug.Log("init health");
+        
+        _postProcess =  Camera.main.GetComponent<PostProcessingBehaviour>();
+        _postColorGrading = _postProcess.profile.colorGrading;
+        _postColorGradingSettings = _postColorGrading.settings;
+        _postColorGradingSettings.basic.saturation = 1f;
+        _postColorGrading.settings = _postColorGradingSettings;
+        _rect = GameOverGui.GetComponent<RectTransform>();
         //gameLifesText.text = lifes.ToString();
+        IsPaused = false;
+        IsGameOver = false;
+        initScreens();
     }
     public void Defeat()
     {
-        IsPaused = true;
+        IsGameOver = true;
+        _defeatEffect = true;
+        SwitchToScreen(TDScreen.GAME_OVER);
+    }
+    public GameObject GameOverGui;
+
+    bool _defeatEffect = false;
+
+    PostProcessingBehaviour _postProcess;
+    ColorGradingModel _postColorGrading;
+    ColorGradingModel.Settings _postColorGradingSettings;
+    float _fadeSaturation = 1.0f;
+    float timescale = 1;
+    RectTransform _rect;
+    void Update()
+    {
+        UpdateTimer();
+        if(_defeatEffect){
+            _rect.localScale = new Vector3(1-timescale,1-timescale,1-timescale);
+            if(Time.timeScale > 0f){
+                timescale =Time.timeScale = Mathf.Clamp(Time.timeScale-0.01f, 0,1);
+            }else if(Time.timeScale == 0){
+                _defeatEffect = false;
+            }
+            if(_fadeSaturation > 0){
+                _fadeSaturation -= 0.01f;
+                _postColorGradingSettings.basic.saturation = _fadeSaturation;
+                _postColorGrading.settings = _postColorGradingSettings;
+            }else if(_fadeSaturation != 0){
+                _fadeSaturation = 0;
+                _postColorGradingSettings.basic.saturation = _fadeSaturation;
+                _postColorGrading.settings = _postColorGradingSettings;
+            }
+        }
     }
     public void Win()
     {
         IsPaused = true;
     }
-    
+    public Dictionary<TDScreen, CanvasGroup> Screens = new Dictionary<TDScreen, CanvasGroup>();
+    public CanvasGroup GameOverScreen;
+    public CanvasGroup PauseScreen;
+    public CanvasGroup HUD;
+
+    void initScreens(){
+        Screens.Add(TDScreen.GAME_OVER, GameOverScreen);
+        Screens.Add(TDScreen.HUD, HUD);
+        Screens.Add(TDScreen.PAUSE, PauseScreen);
+    }
+    public void SwitchToScreen(TDScreen screen){
+        foreach(CanvasGroup canvas in Screens.Values){
+            if(canvas == Screens[screen]){
+                continue;
+            }
+            canvas.interactable = false;
+            canvas.gameObject.SetActive(false);
+        }
+        Screens[screen].interactable = true;
+        Screens[screen].gameObject.SetActive(true);
+    }
+    public void PauseGame(){
+        IsPaused = !IsPaused;
+        Time.timeScale = IsPaused ? 0f:1f;
+        PauseScreen.alpha = IsPaused ? 1f:0f;
+        if(IsPaused){
+            SwitchToScreen(TDScreen.PAUSE);
+        }else{
+            SwitchToScreen(TDScreen.HUD);
+        }
+    }
     public void StartLevel(int mapSizeX, int mapSizeZ, int levelDifficulty, GameObject mapGroundObject)
     {
         Money = _money;        
         MapManager.Instance.createMap(mapSizeX, mapSizeZ, levelDifficulty, mapGroundObject);
         startTime = Time.time;
-        BuildManager.Instance.initGUI();
+        SwitchToScreen(TDScreen.HUD);
     }
 
-    void Update()
-    {
-        UpdateTimer();
-    }
+
     private void UpdateTimer()
     {
-        if (!IsPaused)
+        if (!IsPaused && !IsGameOver)
         {
             disTime = Time.time - startTime;
             string min = Mathf.Floor(disTime / 60).ToString("00");
@@ -97,4 +172,14 @@ public class LevelManager : MonoBehaviour
             gameTimerText.text = min + ":" + sec;
         }
     }
+    void OnApplicationQuit()
+    {
+        _postColorGradingSettings.basic.saturation = 1;
+        _postColorGrading.settings = _postColorGradingSettings;
+    }
+}
+public enum TDScreen{
+    HUD,
+    GAME_OVER,
+    PAUSE
 }
