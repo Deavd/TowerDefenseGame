fc2b8dcf58ec4ea44a4d80be73251d18dda59e4f
David <david.boehm@mng.ch>
2017-10-26 23:32:56 +0200

    made it simpler to get the tower object

--- Assets/TDF/Scripts/Game/BuildManager.cs
+++ Assets/TDF/Scripts/Game/BuildManager.cs
@@ -6,15 +6,16 @@ using UnityEngine.UI;
 public class BuildManager : MonoBehaviour {
 	//TOWER PANEL
     public List<GameObject> towerObjs; //prefab
+    private List<Tower> _towers = new List<Tower>();
     public GameObject towerButton;  //prefab
     public GameObject towerButtonPanelContainer;  //prefab on scene
     public static int selectedTower = -1;
     // END TOWER PANEL
-    static GameObject drawingTower;
+    static GameObject DrawingTower;
     Ray ray;
     RaycastHit hit;
     float towerPreviewUpdateTime = 0.1f;
-    float towerPreviewLastUpdateTime;
+    float towerPreviewLastUpdateTime = 0f;
 
 	private static BuildManager _instance;
     public static BuildManager Instance
@@ -24,13 +25,9 @@ public class BuildManager : MonoBehaviour {
             return (_instance == null ? _instance = FindObjectOfType<BuildManager>() : _instance) == null ? new GameObject().AddComponent<BuildManager>(): _instance;
         }
     }
-	public void initGUI (){
-        LoadTowerBar();
-        enabled = true;
-	}
     void Start()
     {
-        enabled = false;
+        LoadTowerBar();
     }
     void Update()
     {
@@ -40,11 +37,12 @@ public class BuildManager : MonoBehaviour {
             selectedTower = -1;
         }
     }
-    public Tower loadTower(){
-        Debug.Log("SELECTET TOWER: "+selectedTower);
-        return towerObjs[selectedTower].GetComponent<Tower>();
+    public Tower LoadTower(int i){
+        return i != -1 ?_towers[i] : null;
     }
-
+    public Tower LoadTower(){
+        return selectedTower != -1 ?_towers[selectedTower] : null;
+    }
 	private void TowerPreview()
     {
         if(Time.time - towerPreviewLastUpdateTime < towerPreviewUpdateTime){
@@ -53,38 +51,36 @@ public class BuildManager : MonoBehaviour {
         //check if a tower is selected
         if (selectedTower != -1)
         {
-            if (drawingTower != null)
+            if (DrawingTower != null)
             {
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 //check if mouse is over a clickableObj -> may change to another Mask
-                if (Physics.Raycast(ray, out hit, 105f, LayerMask.GetMask("clickableObj")))
+                if (Physics.Raycast(ray, out hit, 105f, LayerMask.GetMask("MAP")))
                 {
                     towerPreviewLastUpdateTime = Time.time;
                     //change prevTower position
                     Vector3 placePosition = MapManager.Instance.getPosition(hit.collider.GetComponent<MapObject>().posX,hit.collider.GetComponent<MapObject>().posZ);
                     //placePosition.y = drawingTower.transform.localScale.y/2;
-                    drawingTower.transform.position = placePosition;
-                    TowerMenuUI.Instance.ShowRangePreview(loadTower().Stats.Range.Value, placePosition); 
+                    DrawingTower.transform.position = placePosition;
+                    TowerMenuUI.Instance.ShowRangePreview(LoadTower().Stats.Range.Value, placePosition); 
                 }
             }
             else
             {
                 //create the prevTower
-                drawingTower = Instantiate(towerObjs[selectedTower], Input.mousePosition, Quaternion.identity);
+                DrawingTower = Instantiate(towerObjs[selectedTower], Input.mousePosition, Quaternion.identity);
             }
         }
-        else if (drawingTower != null)
+        else if (DrawingTower != null)
         {
-            Debug.Log("DE");
-            Destroy(drawingTower);
+            Destroy(DrawingTower);
         }
     }
     public Tower PlaceTower(int x, int z)
     {
-        Tower tower = loadTower();
         //check if player has enough money
         //double buyPrice = tower.BuyPrice;
-        double buyPrice = tower.Stats.BuyPrice.Value;
+        double buyPrice = LoadTower().Stats.BuyPrice.Value;
         
         if (LevelManager.Instance.Money >= buyPrice)
         {
@@ -99,8 +95,7 @@ public class BuildManager : MonoBehaviour {
             NavMeshObstacle navObstacle = towerObj.GetComponent<NavMeshObstacle>();
             navObstacle.enabled = true;
             testTower.Build();
-            Destroy(drawingTower);
-            Debug.Log("REMOVING SELECTED TOWER");
+            Destroy(DrawingTower);
             selectedTower = -1;
             return testTower;
         }
@@ -114,14 +109,10 @@ public class BuildManager : MonoBehaviour {
     //triggered from listener;; select a tower with button in panel
     public void SelectTower(int id)
     {
-        Debug.Log( LevelManager.Instance.Money);
-        Debug.Log( id);
-        Debug.Log( towerObjs[id]);
         //check if player has enough money
         //if (LevelManager.Instance.Money >= towerObjs[id].GetComponent<Tower>().BuyPrice)
-        if (LevelManager.Instance.Money >= towerObjs[id].GetComponent<Tower>().Stats.BuyPrice.Value)
+        if (LevelManager.Instance.Money >= LoadTower(id).Stats.BuyPrice.Value)
         {
-            Debug.Log("SETTING ID TO "+id);
             //change selected tower
             selectedTower = id;
         }
@@ -137,7 +128,7 @@ public class BuildManager : MonoBehaviour {
         foreach (GameObject towerObj in towerObjs)
         {
             Tower tower = towerObj.GetComponent<Tower>();
-            Debug.Log("TOWER INIT: "+tower.displayName+" id: "+id);
+            _towers.Add(tower);
             //create button
             Button button = Instantiate(towerButton).GetComponent<Button>();
             //add a listener
