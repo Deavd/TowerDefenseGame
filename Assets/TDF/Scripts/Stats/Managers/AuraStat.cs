c997650c31b13223a0cf6623efa41ff0d1bf7b0c
David <david.boehm@mng.ch>
2017-12-03 14:13:36 +0100

    stats for the auratower

---
+++ Assets/TDF/Scripts/Stats/Managers/AuraStat.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuraStat : StatManager {
+	public Stat[] AuraStats;
+	protected override void Awake () {
+		foreach(Stat stat in AuraStats){			
+			addStat(stat.Type, stat);
+		}
+		base.Awake();
+	}
+}
