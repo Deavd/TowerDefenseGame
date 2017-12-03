b97ea829271e9c1a28c423f4e839f18a6fe0fe52
David <david.boehm@mng.ch>
2017-12-03 14:06:37 +0100

    cleared

--- Assets/TDF/Scripts/Game/BaseManager.cs
+++ Assets/TDF/Scripts/Game/BaseManager.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 	void OnTriggerEnter(Collider other) {
 		if(other.gameObject.tag == "Enemy"){
 			other.GetComponent<Enemy>().Destinated();
